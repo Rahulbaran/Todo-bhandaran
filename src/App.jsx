441d46ec890f6c8d164647866114497028b68fc6
Rahulkumar <rahulkumar109422@gmail.com>
2023-03-31 15:25:14 +0530

    initialize the project with basic file structure
    - used eslint for javascript linting
    - used stylelint for css linting
    - created scss file structure
    - used vite as bundler

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route
+} from "react-router-dom";
+
+export default function App() {
+  return <RouterProvider></RouterProvider>;
+}
